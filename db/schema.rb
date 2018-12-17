0f94f448be6536e3b7f45eaa7b39310029f5413f
wichru <wicher.maciej@gmail.com>
2018-12-17 18:04:40 +0100

    Create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406223727) do
+ActiveRecord::Schema.define(version: 2018_12_17_160716) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
