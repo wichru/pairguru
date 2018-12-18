82f35176b6bd40d7b5d5f4588520fb2da2c37738
wichru <wicher.maciej@gmail.com>
2018-12-18 02:54:02 +0100

    Allow user to destroy comment

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
     collection do
       get :export
     end
-    resources :comments
+    resources :comments, only: [:create, :destroy]
   end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CommentsController, type: :controller do
   describe "GET #create" do
     it "returns http success" do
       get :create
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
     end
   end
 
@@ -23,6 +23,35 @@ RSpec.describe CommentsController, type: :controller do
       expect do
         post :create, params: { comment: { commenter: 'Joe', body: "What a great movie", movie_id: @movie.id } }
       end.to change(Comment, :count).by(1)
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before do
+      @genre = Genre.create!(name: 'Fancy movie')
+      @movie = Movie.create!(
+        title: "Pulp Fiction",
+        description: "This should be some movie description",
+        genre_id: @genre.id
+      )
+    end
+
+    it "deletes comment" do
+      comment = Comment.create(commenter: 'Joe', body: "What a great movie", movie_id: @movie.id)
+
+      expect do
+        delete :destroy, params: { id: comment }
+      end.to change(Comment, :count).by(-1)
+      expect(response).to be_successful
+    end
+
+    it "deletes comment and redirect" do
+      comment = Comment.create(commenter: 'Joe', body: "What a great movie", movie_id: @movie.id)
+
+      expect do
+        delete :destroy, params: { id: comment }
+      end.to change(Comment, :count).by(-1)
       expect(response).to have_http_status(:redirect)
     end
   end
