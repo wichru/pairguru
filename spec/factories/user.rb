6ae824a83d5ffef78294c0b746af2f9740755a44
wichru <wicher.maciej@gmail.com>
2018-12-19 18:17:46 +0100

    Creates factories for users plus update models validation

---
+++ spec/factories/user.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password "password"
+    confirmed_at 1.day.ago
+  end
+
+  factory :unconfirmed_user do
+    email { Faker::Internet.email }
+    password "password"
+  end
+
+  factory :other_user do
+    email { Faker::Internet.email }
+    password "password"
+    confirmed_at 1.day.ago
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,5 @@ describe User do
   it { is_expected.not_to allow_value("+48 aaa bbb ccc").for(:phone_number) }
   it { is_expected.not_to allow_value("aaa +48 aaa bbb ccc").for(:phone_number) }
   it { is_expected.not_to allow_value("+48 999 888 777\naseasd").for(:phone_number) }
+  it { should have_many(:comments).dependent(:destroy) }
 end
